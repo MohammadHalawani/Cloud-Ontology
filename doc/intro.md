1ab5b125751b2ed551aa2c211aa47cc4e7350922
Mohammad Halawani <32101730+MohammadHalawani@users.noreply.github.com>
2023-03-10 18:11:26 +0000

    Update files
    Update .gitignore
    Update Readme
    Update clojure code
    Move directory back

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clouds
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clouds/core_test.clj
@@ -0,0 +1,7 @@
+(ns clouds.core-test
+  (:require [clojure.test :refer :all]
+            [clouds.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
