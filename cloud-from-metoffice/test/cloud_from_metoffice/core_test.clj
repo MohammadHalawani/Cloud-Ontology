be3097608588c96cc746801586d201d6cd47f626
Mohammad Halawani <32101730+MohammadHalawani@users.noreply.github.com>
2023-03-10 18:25:38 +0000

    Delete cloud-from-metoffice directory

--- cloud-from-metoffice/test/cloud_from_metoffice/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cloud-from-metoffice.core-test
-  (:require [clojure.test :refer :all]
-            [cloud-from-metoffice.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
